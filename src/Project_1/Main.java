d09128a27c2905b394c39091cc7259bd5cca9805
heman <hemanth37906@gmail.com>
2025-03-10 19:12:48 +0530

    added package and customer ,order class

--- src/Project_1/Main.java
+++ src/Project_1/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) {
 
         Customer customer_1 = new Customer(7, "hem", "jkalf@gmail.er");
-        customer_1.setId(88);
-        customer_1.setName("jsdf");
 
         String email = customer_1.getEmail();
+        Order order = new Order(7,true,72000);
+        System.out.println(order.getIsDelivered());
 
         System.out.println(email);
 
